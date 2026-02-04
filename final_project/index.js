f25920c7005b05111548fee8785fec10396a5570
g ve <gloria.laura.vela@gmail.com>
2026-02-04 16:06:24 -0500

    Commit task 6

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { isValid } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -32,25 +33,6 @@ app.use("/customer/auth/*", function auth(req, res, next) {
     }
 });
 
-app.post("/register/:username/:password", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // Validate that both username and password are provided
-    if (username && password) {
-        // Ensure the username is not already registered
-        if (!authenticatedUser(username, password)) {
-            // Add the new user to the in-memory users array
-            users.push({ "username": username, "password": password });
-            return res.status(200).json({ message: "User successfully registered. Now you can login" });
-        } else {
-            // Return error if username is already taken
-            return res.status(404).json({ message: "User already exists!" });
-        }
-    }
-    // Return error if username or password is missing
-    return res.status(404).json({ message: "Unable to register user." });
-});
 
 const PORT = 5000;
 
