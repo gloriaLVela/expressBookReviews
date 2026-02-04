564078a242e6e6f93999ff0da7e68c23a69d0d5a
g ve <gloria.laura.vela@gmail.com>
2026-02-04 13:17:44 -0500

    Completed second task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,8 +18,12 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const book = books[req.params.isbn];
+    if (book) {
+        res.send(JSON.stringify(book, null, 4));
+    } else {
+        res.send("Book not found");
+    }
  });
   
 // Get book details based on author
